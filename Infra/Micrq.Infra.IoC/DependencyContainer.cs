15fea58376c141c860c407f5f7c463970c267c20
iamiri <i.amiri@derun.virtux.nl>
2023-01-29 12:07:39 +0330

    Add ioc to project

---
+++ Infra/Micrq.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MicRq.Infra.Bus;
+
+namespace Micrq.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services) 
+        {
+            services.AddTransient<IEventBus, RabbitMqBus>();
+        }
+    }
+}
